e1f1ce17e62e652fe0fa0a437197b247e3ce4f1a
Yash Tank <69433053+Yashtank-git@users.noreply.github.com>
2024-04-17 20:57:07 +0530

    "Edit sonar install file"

--- terraform/01-AWS-IaS/sonarqube-install.sh
+++ terraform/01-AWS-IaS/sonarqube-install.sh
@@ -1,9 +1,10 @@
 sudo apt update -y
 sudo apt install openjdk-17-jre unzip -y
 sudo groupadd sonarqube
+cd /home/ubuntu
 wget "https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.9.4.87374.zip"
 unzip sonarqube-9.9.4.87374
 chown -R ubuntu:sonarqube sonarqube-9.9.4.87374
 sudo chmod -R 755 sonarqube-9.9.4.87374
 cd sonarqube-9.9.4.87374/bin/linux-x86-64
-./sonar.sh start
+sudo ./sonar.sh start
