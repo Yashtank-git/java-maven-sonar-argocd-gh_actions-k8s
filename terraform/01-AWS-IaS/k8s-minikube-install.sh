a990a58a31364bb141d92203fe6daf5616034f80
Yash Tank <69433053+Yashtank-git@users.noreply.github.com>
2024-04-18 23:42:52 +0530

    Update k8s-minikube-install.sh

--- terraform/01-AWS-IaS/k8s-minikube-install.sh
+++ terraform/01-AWS-IaS/k8s-minikube-install.sh
@@ -11,9 +11,12 @@ sudo usermod -aG sudo ubuntu
 cd /home/ubuntu
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
+sudo usermod -aG docker $USER && newgrp docker
 minikube start --driver=docker
 
 #Setting kubectl to run as 'minikube kubectl'
 alias kubectl="minikube kubectl --"
 ln -s $(which minikube) /usr/local/bin/kubectl
+
+
 
